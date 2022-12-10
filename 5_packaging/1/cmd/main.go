c624dfa178cfd528f1cd4b8b4df132f181026fd7
Polidoro-root <jv.polidoro@outlook.com>
2022-12-10 10:15:56 -0300

    Using custom module

---
+++ 5_packaging/1/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Polidoro-root/go-expert-classes/5_packaging/1/math"
+)
+
+func main() {
+	m := math.Math{A: 1, B: 2}
+
+	fmt.Print(m.Add())
+
+	// fmt.Println("hello world")
+}
--- 5_packaging/1/go.mod
+++ 5_packaging/1/go.mod
@@ -1,3 +1,5 @@
 module github.com/Polidoro-root/go-expert-classes/5_packaging/1
 
 go 1.19
+
+
