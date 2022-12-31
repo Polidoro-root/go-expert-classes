8dd67fccfcbc8ddd9208413ea3b213c486d06934
Polidoro-root <jv.polidoro@outlook.com>
2022-12-31 17:17:02 -0300

    CreateProduct handler

--- 7_apis/cmd/server/main.go
+++ 7_apis/cmd/server/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "github.com/Polidoro-root/go-expert/7_apis/configs"
+import (
+	"net/http"
+
+	"github.com/Polidoro-root/go-expert/7_apis/configs"
+	"github.com/Polidoro-root/go-expert/7_apis/internal/entity"
+	"github.com/Polidoro-root/go-expert/7_apis/internal/infra/database"
+	"github.com/Polidoro-root/go-expert/7_apis/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8080", nil)
 }
