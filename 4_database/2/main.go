618dd586293841170b7d5cc2f4e5b21f6b09c200
Polidoro-root <jv.polidoro@outlook.com>
2023-04-05 22:42:37 -0300

    Create GORM mod

---
+++ 4_database/2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/expert"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{
+		Name:  "Notebook",
+		Price: 1000.0,
+	})
+
+	var product Product
+
+	db.First(&product, 1)
+}
